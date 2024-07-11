0be0b102ef0015594c213ba1635b67cf307dff3a
jiayi <jyhee@nh2system.com>
2024-07-11 11:26:12 +0800

    Update with action workflow + build setup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 const path = require('path')
 
+const nextConfig = {
+  basePath: "/next-ecommerce",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
+
+/*
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
 }
+*/
